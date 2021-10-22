5d56ac65416a3236d61ce1d4c09051ee787f8c51
Alexey Uzlov <auzlov@codemastersintl.com>
2021-10-22 19:33:56 +0300

    * added angular for admin

--- admin/karma.conf.js
+++ admin/karma.conf.js
@@ -1,110 +1,44 @@
-const testWebpackConfig = require('./webpack.dev.js')({env: 'test'});
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-    config.set({
-        /**
-         * Base path that will be used to resolve all patterns (e.g. files, exclude).
-         */
-        basePath: '.',
-
-        /**
-         * Frameworks to use
-         *
-         * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-         */
-        frameworks: ['jasmine'],
-
-        /**
-         * List of files to exclude.
-         */
-        exclude: [],
-
-        /**
-         * List of files / patterns to load in the browser
-         *
-         * we are building the test environment in ./spec-bundle.js
-         */
-        files: [
-            {pattern: './spec-bundle.js', watched: false},
-        ],
-
-        /**
-         * Preprocess matching files before serving them to the browser
-         * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-         */
-        preprocessors: {
-            // add webpack as preprocessor
-            'spec-bundle.js': ['webpack']
-        },
-
-        /**
-         * Test results reporter to use
-         *
-         * possible values: 'dots', 'progress'
-         * available reporters: https://npmjs.org/browse/keyword/karma-reporter
-         */
-        reporters: ['mocha'],
-
-        /**
-         * Webpack Config at ./webpack.test.js
-         */
-        webpack: testWebpackConfig,
-
-        /**
-         * Webpack please don't spam the console when running in karma!
-         */
-        webpackMiddleware: {
-            /**
-             * webpack-dev-middleware configuration
-             * i.e.
-             */
-            noInfo: true,
-            /**
-             * and use stats to turn off verbose output
-             */
-            stats: {
-                /**
-                 * options i.e.
-                 */
-                chunks: false
-            }
-        },
-
-        /**
-         * enable / disable colors in the output (reporters and logs)
-         */
-        colors: true,
-
-        /**
-         * Level of logging
-         * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-         */
-        logLevel: config.LOG_WARN,
-
-        /**
-         * enable / disable watching file and executing tests whenever any file changes
-         */
-        autoWatch: false,
-
-        /**
-         * start these browsers
-         * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-         */
-        browsers: [
-            'ChromeHeadless',
-        ],
-
-        customLaunchers: {
-            ChromeHeadless: {
-                base: 'Chrome',
-                flags: ['--headless', '--remote-debugging-port=9222']
-            }
-        },
-
-        /**
-         * Continuous Integration mode
-         * if true, Karma captures browsers, runs the tests and exits
-         */
-        singleRun: true,
-    })
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/sample'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
 };
--- admin/src/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-import 'bootstrap';
-
-import '../styles/style.scss';
-import { DateUtils } from './app/utils/date/date';
-
-const date = DateUtils.now();
-console.info(date);
