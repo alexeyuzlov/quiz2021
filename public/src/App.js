b2071b4afe4859960eae1c5f7f754a8490a87828
Alexey Uzlov <auzlov@codemastersintl.com>
2021-10-23 08:42:50 +0300

    * added more react features

--- public/src/App.js
+++ public/src/App.js
@@ -1,27 +1,52 @@
 import './App.css';
 import Quiz from "./Quiz";
+import {Component} from "react";
 
-function App() {
-    return (
-        <div className="page">
-            <header className="header page__header">
-                <h1>Quiz</h1>
-            </header>
-            <div className="page__content main">
-                <aside className="main__sidebar sidebar">
-                    list
-                </aside>
-                <main className="main__content">
-                    <Quiz/>
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-                    <div className="hide">Вы ответили правильно на X из X вопросов</div>
-                </main>
+        this.state = {
+            isStarted: true
+        }
+    }
+
+    toggle() {
+        this.setState((state) => ({
+            isStarted: !state.isStarted
+        }))
+    }
+
+    render() {
+        return (
+            <div className="page">
+                <header className="header page__header">
+                    <h1>Quiz</h1>
+                </header>
+                <div className="page__content main">
+                    <aside className="main__sidebar sidebar">
+                        list
+                    </aside>
+                    <main className="main__content">
+                        {this.state.isStarted ? 'true' : 'false'}
+
+                        {
+                            this.state.isStarted
+                                ?
+                                <Quiz title={'Сколько пальцев на руке?'} oneMore={'123'}/>
+                                :
+                                <div>Вы ответили правильно на X из X вопросов</div>
+                        }
+
+                        <button type="button" onClick={() => this.toggle()}>Toggle</button>
+                    </main>
+                </div>
+                <footer className="footer page__footer">
+                    Alexey Uzlov, 2021
+                </footer>
             </div>
-            <footer className="footer page__footer">
-                Alexey Uzlov, 2021
-            </footer>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- public/src/Quiz.jsx
+++ public/src/Quiz.jsx
@@ -1,14 +1,34 @@
-function Quiz() {
-    return (
-        <div className="quiz-form">
-            <h2>Question text?</h2>
+import {Component} from "react";
 
-            <form action="">
-                <input type="text"/>
-                <button type="submit">Отправить</button>
-            </form>
-        </div>
-    );
+class Quiz extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            answer: ''
+        }
+    }
+
+    save(e) {
+        this.setState({answer: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="quiz-form">
+                <h2>{this.props.title}</h2>
+
+                <form action="">
+                    <input
+                        value={this.state.answer}
+                        type="text"
+                        onChange={(e) => this.save(e)}
+                    />
+                    <button type="submit">Отправить</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Quiz;
